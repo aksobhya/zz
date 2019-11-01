7544449078cb910be0c6b6b17904cb503b8c78ed
aksobhya <aksobhya@163.com>
2019-11-01 17:43:32 +0800

    elements.change

--- kuaishoujisuban.js
+++ kuaishoujisuban.js
@@ -2,20 +2,31 @@
     var elements = {
         AppName:"kuaishoujisuban",
         packageName:"com.kuaishou.nebula",
-        signin:{
-            threebar:'className("android.widget.ImageView").id("left_btn")'
+        home:{
+            threebar:'className("android.widget.ImageView").id("left_btn")',
+            threepoint:'className("android.view.View").id("comment_icon")'
         },
+        youngmod:{
+
+
+
+        },
+        invite:{
+            close:'className("android.widget.ImageButton").id("close")',
+            lijiyaoqing:'className("android.widget.TextView).id("negative")'
+
+        }
     }
 
     var sac = {util:require("./util.js")};
 
     sac.util.clean();
     sac.util.openApp(elements.packageName);
-    sleep(10000);
+    sleep(10000);  // Open app , look around and decide to signin.
 
-    let where = sac.util.grope(elements.signin, "threebar");
-    if(where("threebar", 5000)){
-        sac.util.forcePress(elements.signin.threebar);
+    let where = sac.util.gropev2(elements);
+    if(where("home", 5000)){
+        sac.util.forcePress(elements.home.threebar);
     }else{
         sac.util.print("Home to Signin, Error!", 1);
         return ;
